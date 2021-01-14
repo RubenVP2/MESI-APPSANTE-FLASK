e91c3ce5d99baa18885ca880875792ce69e3f0c0
Ruben VP <paulos.ruben@gmail.com>
2021-01-14 17:38:34 +0100

    Ajout des CORS

--- app.py
+++ app.py
@@ -2,11 +2,31 @@ import sqlite3
 import click
 
 from flask import Flask, g, json
-from flask_cors import CORS
+
+# from flask_cors import CORS
 from flask.cli import with_appcontext
 
 app = Flask(__name__)
-cors = CORS(app)
+# cors = CORS(app)
+
+
+"""
+Enable CORS. Disable it if you don't need CORS
+https://parzibyte.me/blog
+"""
+
+
+@app.after_request
+def after_request(response):
+    response.headers[
+        "Access-Control-Allow-Origin"
+    ] = "*"  # <- You can change "*" for a domain for example "http://localhost"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    response.headers[
+        "Access-Control-Allow-Headers"
+    ] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    return response
 
 
 @app.route("/")
@@ -15,6 +35,7 @@ def index():
 
 
 # Cette route ne sert qu'a montrer comment faire. Eviter de l'utiliser surtout quand y'aura beaucoup d'utilisateur !!!
+
 
 @app.route("/test/user/all")
 def all_users():
@@ -34,6 +55,7 @@ def user(idUser: int):
     Partie BDD
 """
 
+
 def get_all_users():
     """ Return information of all the user """
     return make_query(
@@ -42,6 +64,7 @@ def get_all_users():
         FROM USER""",
         0,
     )
+
 
 def get_user(idUser: int):
     """ Return information of the user """
@@ -52,6 +75,7 @@ def get_user(idUser: int):
         WHERE id_user = {idUser}""",
         0,
     )
+
 
 def make_query(query: str, needCommit: bool):
     """ Execute la requête passé en paramètre """
